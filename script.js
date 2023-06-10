86adf025b5f39c8e2d07ee616d5e1fb11b94fda2
Arpan840 <123809535+Arpan840@users.noreply.github.com>
2023-06-10 12:40:58 -0500

    created a submission

--- script.js
+++ script.js
@@ -32,10 +32,20 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  let decodedArr = []; 
+	for(i=0;i<encodedStr.length;i++)
+		{
+			let char=encodedStr[i].toUpperCase();
+			if(lookup[char]===undefined)
+			{
+				decodedArr.push(char)
+			}
+			else{
+				decodedArr.push(lookup[char]);
+			}
+		}
 
-  return; //return decodedArr
+  return decodedArr;
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
