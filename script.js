ddfc13ca12140506a2ee725a013ad2ef77ab413a
Arpan840 <123809535+Arpan840@users.noreply.github.com>
2023-06-10 12:51:47 -0500

    created a submission

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ function rot13(encodedStr) {
   let decodedArr = []; 
 	for(i=0;i<encodedStr.length;i++)
 		{
+			let char=encodedStr[i]
 			if(lookup[char]===undefined)
 			{
 				decodedArr.push(char)
@@ -44,7 +45,7 @@ function rot13(encodedStr) {
 			}
 		}
 
-  return decodedArr;
+  return decodedArr.join("");
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
